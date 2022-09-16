2787fd1afea75d9aae0ace02055ed8d85bf2d359
unknown <nathan.renovato300@gmail.com>
2022-09-16 13:35:13 -0300

    Resolution exercise 04

--- classe-04/ex4.js
+++ classe-04/ex4.js
@@ -2,7 +2,11 @@ const classA = ["Daniel", "Guido", "Léo", "Vitor", "Ruliana", "Diego", "José"]
 const classB = ["Marcos", "Maria", "Juliana", "Cristina", "Paula"];
 const classC = ["Marcela", "Pedro", "João", "Jorge", "Carlos", "Henrique"];
 
-let allClasses = [...classA, ...classB, ...classC];
+let allClasses = [...classA, ...classB];
+
+const [zero, one, , , four, five] = classC;
+
+allClasses = [...allClasses, zero, one, four, five];
 
 console.log(allClasses);
 
